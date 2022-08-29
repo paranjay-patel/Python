107d0f69b8e24180efbf1c76780bf78a35c5c6e7
paranjay <pearlpatel1234@gmail.com>
2022-08-29 10:11:48 +0530

    added highScore in game

--- snake_game/main.py
+++ snake_game/main.py
@@ -33,24 +33,23 @@ while game_is_on:
     #detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
-        scoreboard.clear()
         snake.extend()
         scoreboard.increse_score()
   
     #detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on= False
         print("collide with wall")
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #detect collision with tail
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
             print("collide")
     #if head collides with any segment in the tail
 
-screen.exitonclick()
+screen.exitonick()
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -3,19 +3,35 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        with open("snake_game\data.txt",mode="r") as file:
+            high_score = file.read()
+            print(f"high_score type : {type(high_score)}")
         self.score = 0
+        self.highest_score = high_score
         self.color("white")
         self.penup()
-        self.goto(0,270)
-        self.write(f"Score: {self.score}",align="center",font=("Arial",24,"normal"))
+        self.goto(0,250)
         self.hideturtle()
+        self.write(f"Score: {self.score}  highest: {self.highest_score}",align="center",font=("Arial",24,"normal"))
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}  highest: {self.highest_score}",align="center",font=("Arial",24,"normal"))
 
     def increse_score(self):
         self.score += 1
-        self.write(f"Score: {self.score}",align="center",font=("Arial",24,"normal"))
+        self.update_scoreboard()
 
-    def game_over(self):
-        print("game over")
-        self.color("blue")
-        self.goto(0,0)
-        self.write("GAME OVER",align="center",font=("Arial",24,"normal"))
+    def reset(self):
+        if self.score > int(self.highest_score):
+            self.highest_score = self.score
+        with open("snake_game\data.txt",mode="w") as file:
+            file.write(str(self.highest_score))
+        self.score = 0
+        self.update_scoreboard()   
+    
+    # def game_over(self):
+    #     print("game over")
+    #     self.color("blue")
+    #     self.goto(0,0)
+    #     self.write("GAME OVER",align="center",font=("Arial",24,"normal"))
